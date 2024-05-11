cc511e8ea72905d2fff728152dc08d138b54dec0
Arun sd <146158684+Arun1SD@users.noreply.github.com>
2024-05-11 14:00:42 +0530

    read inputs from user

---
+++ addtwointeger.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+int main()
+{
+    int a,b,c;
+
+    printf("enter the 1st number to add : ");
+    scanf("%d",&a);
+
+    printf("enter the second number to add : ");
+    scanf("%d",&b);
+    
+    c=a+b;
+
+    printf("addition of integer is : %d",c);
+
+    return 0;
+
+}
